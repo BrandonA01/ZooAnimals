3196426c05b7b5768d9a1bfcbe06aa9088a2abd2
Brandon Allen <81233247+BrandonA01@users.noreply.github.com>
2022-08-12 09:38:43 +0100

    Initial Commit

---
+++ src/main/java/com/softwareinstitute/allen/brandon/ZooAnimals/ZooAnimalsApplication.java
@@ -0,0 +1,13 @@
+package com.softwareinstitute.allen.brandon.ZooAnimals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooAnimalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooAnimalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwareinstitute/allen/brandon/ZooAnimals/ZooAnimalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareinstitute.allen.brandon.ZooAnimals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooAnimalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
